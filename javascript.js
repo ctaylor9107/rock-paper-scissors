eeb8bf782c19916646c7192c2ccf1231f69f611d
ctaylor9107 <coleman.raphael.taylor@gmail.com>
2025-02-23 09:04:58 -0600

    Create function allowing for human choice
    Created a function to allow the human player to
    make a choice in the Rock, Paper, Scissors game
    
    The player can make a choice between Rock, Paper,
    or Scissors without regards to capitalization
    
    If th user picks something else than the above
    three choices it will prompt accordingly

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 
 
-// Create function to choose computer's
-// choice in Rock, Paper, Scissors game
+// Function to choose computer's choice
+// in Rock, Paper, Scissors game
 
 
 function getComputerChoice() {
@@ -21,3 +21,37 @@ function getComputerChoice() {
 
 
 console.log(getComputerChoice());
+
+
+// Function to allow the human player to make a choice
+// in the Rock, Paper, Scissors game
+
+
+function getHumanChoice() {
+    let humChoice = prompt("Choose one: Rock, Paper, or Scissors")
+
+    if (humChoice === null) {
+        return "Try again"
+    }
+    else if (humChoice.toLowerCase() === "") {
+        return "Don't be afraid, you can beat the computer"
+    }
+    else if (humChoice.toLowerCase() === "rock") {
+        return "Rock"
+    }
+    else if (humChoice.toLowerCase() === "scissors") {
+        return "Scissors"
+    }
+    else if (humChoice.toLowerCase() === "paper") {
+        return "Paper"
+    }
+    else {
+        return "Please enter a valid choice"
+    }
+}
+
+
+console.log(getHumanChoice());
+
+
+
