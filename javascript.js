3b170988033c8ddcbd5ccbc3998d81aeccf5577b
ctaylor9107 <coleman.raphael.taylor@gmail.com>
2025-03-07 18:55:46 -0600

    Capture player button input and return results
    Added an event so that what the player chooses
    from the buttons is captured and run through the
    playRound function to determine who won the round.
    
    The choices and results are currently displayed in
    the console but will be displaying on the page in
    the next iteration.

--- javascript.js
+++ javascript.js
@@ -25,33 +25,70 @@ function getComputerChoice() {
 // in the Rock, Paper, Scissors game
 
 
-function getHumanChoice() {
-    let humChoice = prompt("Choose one: Rock, Paper, or Scissors.\nFirst to 5 points wins.")
+// function getHumanChoice() {
+//     let humChoice = prompt("Choose one: Rock, Paper, or Scissors.\nFirst to 5 points wins.")
 
-    if (humChoice === null) {
-        return "Try again"
-    }
-    else if (humChoice.toLowerCase() === "") {
-        return "Don't be afraid, you can beat the computer"
-    }
-    else if (humChoice.toLowerCase() === "rock") {
-        return "Rock"
-    }
-    else if (humChoice.toLowerCase() === "scissors") {
-        return "Scissors"
-    }
-    else if (humChoice.toLowerCase() === "paper") {
-        return "Paper"
-    }
-    else {
-        return "Please enter a valid choice."
-    }
-}
+//     if (humChoice === null) {
+//         return "Try again"
+//     }
+//     else if (humChoice.toLowerCase() === "") {
+//         return "Don't be afraid, you can beat the computer"
+//     }
+//     else if (humChoice.toLowerCase() === "rock") {
+//         return "Rock"
+//     }
+//     else if (humChoice.toLowerCase() === "scissors") {
+//         return "Scissors"
+//     }
+//     else if (humChoice.toLowerCase() === "paper") {
+//         return "Paper"
+//     }
+//     else {
+//         return "Please enter a valid choice."
+//     }
+// }
+
+// const rock = document.querySelector("#rock");
+// const paper = document.querySelector("#paper");
+// const scissors = document.querySelector("#scissors");
+
+const buttons = document.querySelectorAll("button");
+
+let playerSelection
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playerSelection = button.textContent;
+        console.log(playerSelection);
+        console.log(playRound(playerSelection));
+        console.log(humanScore, computerScore);
+    });
+});
+
+
+
+// function getHumanChoice () {
+//     if (btn.textContent === "Rock") {
+//         return "Rock"
+//     }
+//     else if (btn.textContent === "Scissors") {
+//         return "Scissors"
+//     }
+//     else if (btn.textContent === "Paper") {
+//         return "Paper"
+//     }
+//     else {
+//         "Please enter a valid choice"
+//     }
+// };
+
+
+
 
 // Function to play the game, which is playing rounds of rock, paper, scissors until
 // either player reaches 5 points
 
-function playGame() {
+//function playGame() {
 
 
     // Variables to track human and computer scores
@@ -61,14 +98,18 @@ function playGame() {
 
     //function to determine if the game needs to be replayed or if it is over
 
-    function triggerReplay(){
+    //function triggerReplay(){
     
     // Function to determine who wins the round based on human and computer choices
     // and increment the score by 1 for whoever won the round
 
-        function playRound() {
 
-            let humanChoice = getHumanChoice();
+    
+        
+    
+        function playRound(playerChoice) {
+
+            let humanChoice = playerChoice;
             let computerChoice = getComputerChoice();
             
             if ((humanChoice === "Rock" && computerChoice === "Rock")
@@ -106,13 +147,19 @@ function playGame() {
             else if (humanChoice === null) {
                 return "Try again."
             }
+
+            
         }
+
+        
+    
+
        
 
 
     // Display the message declaring outcome of the round and track scores    
 
-    console.log(playRound());
+    //console.log(playRound());
     console.log("Player Score: " + humanScore, "Computer Score: " + computerScore)
 
     // Function to determine if either score is at 3 yet, is triggered, and placed
@@ -128,43 +175,43 @@ function playGame() {
         }
     }
 
-    playAgain();
+    //playAgain();
 
     let replay = playAgain();
 
     // Based on above variable, the below will trigger the game again or end
 
     
-    if (replay === "Yes") {
-       triggerReplay();
-    }
-    else if (replay === "No") {
-          "Game Over"
-    }
+    // if (replay === "Yes") {
+    //    triggerReplay();
+    // }
+    // else if (replay === "No") {
+    //       "Game Over"
+    // }
     
 
-    }
+    //}
 
     // Enacts the above code to run through the round and determines if it needs
     // to run through it again or not
 
-    triggerReplay();
+    //triggerReplay();
 
     // Once a score has reached 5, one of the below messages will be displayed
     // based on who won
     
-    if (humanScore > computerScore) {
-        return "Congratulations, you got to 5 points first. You win!"
-    }
-    else if (computerScore > humanScore) {
-        return "Better luck next time.  The computer got to 5 points first so you lost.\nRefresh to try again."
-    }
+    // if (humanScore > computerScore) {
+    //     return "Congratulations, you got to 5 points first. You win!"
+    // }
+    // else if (computerScore > humanScore) {
+    //     return "Better luck next time.  The computer got to 5 points first so you lost.\nRefresh to try again."
+    // }
     
 
        
 
-}
+//}
 
 // Calls the playGame function and displays the winner once it is over
 
-console.log(playGame());
+//console.log(playGame());
